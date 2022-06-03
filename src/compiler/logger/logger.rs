3728195ada6e069e85aa13e43d8c381451dea11e
Ph0enixKM <pkaras.it@gmail.com>
2022-06-03 21:48:35 +0200

    feat: make logger show errors, warnings, infos

--- src/compiler/logger/logger.rs
+++ src/compiler/logger/logger.rs
@@ -1,8 +1,9 @@
+use colored::Colorize;
 use crate::compiler::Token;
 
 pub enum LogType {
-    Err,
-    Warn,
+    Error,
+    Warning,
     Info
 }
 
@@ -11,8 +12,8 @@ pub struct Log<'a> {
     pub row: usize,
     pub col: usize,
     pub path: &'a String,
-    pub message: Option<String>,
-    pub comment: Option<String>
+    pub message_content: Option<String>,
+    pub comment_content: Option<String>
 }
 
 impl<'a> Log<'a> {
@@ -22,17 +23,17 @@ impl<'a> Log<'a> {
             path,
             row,
             col,
-            message: None,
-            comment: None
+            message_content: None,
+            comment_content: None
         }
     }
 
     pub fn new_err(path: &'a String, row: usize, col: usize) -> Self {
-        Log::new(path, row, col, LogType::Err)
+        Log::new(path, row, col, LogType::Error)
     }
 
     pub fn new_warn(path: &'a String, row: usize, col: usize) -> Self {
-        Log::new(path, row, col, LogType::Warn)
+        Log::new(path, row, col, LogType::Warning)
     }
 
     pub fn new_info(path: &'a String, row: usize, col: usize) -> Self {
@@ -52,13 +53,48 @@ impl<'a> Log<'a> {
     }
 
     pub fn message<T: AsRef<str>>(mut self, text: T) -> Self {
-        self.message = Some(String::from(text.as_ref()));
+        self.message_content = Some(String::from(text.as_ref()));
         self
     }
 
     pub fn comment<T: AsRef<str>>(mut self, text: T) -> Self {
-        self.comment = Some(String::from(text.as_ref()));
+        self.comment_content = Some(String::from(text.as_ref()));
         self
+    }
+
+    pub fn send(&self) {
+        match &self.kind {
+            LogType::Error => {
+                println!("{}", format!(" ERROR ").truecolor(0, 0, 0).bold().on_truecolor(255, 80, 80));
+                if let Some(message) = &self.message_content {
+                    println!("{}", format!("{}", message).red());
+                    println!("{}", format!("at {}:{}:{}", self.path, self.row, self.col).red().dimmed());
+                }
+                if let Some(comment) = &self.comment_content {
+                    println!("\n{}", format!("{}", comment).red());
+                }
+            },
+            LogType::Warning => {
+                println!("{}", format!(" WARN ").truecolor(0, 0, 0).bold().on_truecolor(255, 180, 80));
+                if let Some(message) = &self.message_content {
+                    println!("{}", format!("{}", message).yellow());
+                    println!("{}", format!("at {}:{}:{}", self.path, self.row, self.col).yellow().dimmed());
+                }
+                if let Some(comment) = &self.comment_content {
+                    println!("\n{}", format!("{}", comment).yellow());
+                }
+            },
+            LogType::Info => {
+                println!("{}", format!(" INFO ").truecolor(0, 0, 0).bold().on_blue());
+                if let Some(message) = &self.message_content {
+                    println!("{}", format!("{}", message).blue());
+                    println!("{}", format!("at {}:{}:{}", self.path, self.row, self.col).blue().dimmed());
+                }
+                if let Some(comment) = &self.comment_content {
+                    println!("\n{}", format!("{}", comment).blue());
+                }
+            }
+        };
     }
 }
 
