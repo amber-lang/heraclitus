9ae145ebb41dfc7dd163b39814fd94ac6da9a4f3
Ph0enixKM <pkaras.it@gmail.com>
2022-06-16 20:37:27 +0200

    fix: remove context

--- src/rules/rules.rs
+++ src/rules/rules.rs
@@ -1,12 +1,9 @@
 use super::region::Region;
-use super::context::Context;
 
 pub struct Rules {
     pub symbols: Vec<char>,
     pub region_tree: Region,
     pub escape_symbol: char,
-    pub scopes: Vec<Context>,
-    pub global_scope: Option<Context>
 }
 
 impl Rules {
@@ -14,19 +11,8 @@ impl Rules {
         Rules {
             symbols,
             region_tree,
-            escape_symbol: '\\',
-            scopes: vec![],
-            global_scope: None
+            escape_symbol: '\\'
         }
-    }
-
-    pub fn attach_scopes(mut self, scopes: Vec<Context>) -> Self {
-        self.scopes = scopes;
-        match self.scopes.iter().find(|item| item.global) {
-            Some(global) => self.global_scope = Some(global.clone()),
-            None => panic!("Could not find global context")
-        }
-        self
     }
 
     pub fn set_escape(mut self, symbol: char) -> Self {
