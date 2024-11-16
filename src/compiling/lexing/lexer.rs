1f93984dbe5e8a670ac2ff112e8a7a2b6667b16f
b1ek <me@blek.codes>
2024-11-16 21:20:22 +1000

    fix serde not compiling for struct with lifetime

--- src/compiling/lexing/lexer.rs
+++ src/compiling/lexing/lexer.rs
@@ -33,13 +33,13 @@ pub struct Lexer {
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-struct LexState<'a> {
+struct LexState {
     word: String,
     is_indenting: bool,
     is_escaped: bool,
     token_start_index: usize,
     position: (usize, usize),
-    reader: Reader<'a>,
+    reader: Reader,
     lexem: Vec<Token>,
     region_handler: RegionHandler,
     compound_handler: CompoundHandler,
--- src/compiling/lexing/reader.rs
+++ src/compiling/lexing/reader.rs
@@ -10,18 +10,18 @@ pub enum ReadMode {
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct Reader<'a> {
-    pub code: &'a String,
+pub struct Reader {
+    pub code: String,
     pub row: usize,
     pub col: usize,
     pub index: usize,
     pub new_line: bool
 }
 
-impl<'a> Reader<'a> {
-    pub fn new(code: &'a String) -> Self {
+impl Reader {
+    pub fn new(code: &String) -> Self {
         Reader {
-            code,
+            code: code.clone(),
             row: BEGINNING.0,
             col: BEGINNING.1,
             index: 0,
@@ -116,7 +116,7 @@ impl<'a> Reader<'a> {
     }
 }
 
-impl<'a> Iterator for Reader<'a> {
+impl Iterator for Reader {
     type Item = char;
 
     fn next(&mut self) -> Option<Self::Item> {
